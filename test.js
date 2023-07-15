8550d23f45fadb3a99e9a73121b9ff5e2519d230
Samuel J <51910006+muceres@users.noreply.github.com>
2023-07-15 14:20:02 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+const __NAME__ = require(".");
+
+describe("__NAME__ tests", () => {
+  test("if __NAME__ is called then it must be defined", () => {
+    expect(typeof dummyFunction).toBe("function");
+  });
+});
